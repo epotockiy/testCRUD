2f40b29408f178fb3c4b95dcc5720fa760c5c0c7
ezhuk <zhuk.evgeniy97@gmail.com>
2016-07-21 17:33:29 +0300

    Routing has been added.Problem with requests is solving

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+'use strict';
+
+const browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const bower = require('gulp-bower');
+const notify = require('gulp-notify');
+const rename = require('gulp-rename');
+const minify = require('gulp-minify-css');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const cssmin = require('gulp-cssmin');
+
+var config = {
+    bootstrapDir: './vendor/bower_components/bootstrap-sass',
+    boostrapValidatorDir: './vendor/bower_components/bootstrap-validator',
+    publicDir: './assets',
+    bowerDir: './vendor/bower_components',
+    sassPath: './resource',
+    jQueryDir: './vendor/bower_components/jquery',
+    knockoutDir: './vendor/bower_components/knockout',
+    koMappingDir: './vendor/bower_components/bower-knockout-mapping/',
+    pagerJsDir: './node_modules/pagerjs'
+};
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+});
+
+gulp.task('bower', function() {
+    return bower()
+        .pipe(gulp.dest(config.bowerDir))
+});
+
+gulp.task('sass', function() {
+    return gulp.src(config.sassPath + '/main.scss')
+        .pipe(sass({
+            includePaths: [config.bootstrapDir + '/assets/stylesheets']
+        }))
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .on('error', notify.onError(function(error) {
+            return "Error :" + error.message;
+        }))
+        .pipe(gulp.dest(config.publicDir + '/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('fonts', function() {
+    return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+        .pipe(gulp.dest(config.publicDir + '/fonts'));
+});
+
+gulp.task('uglifyPlugins', function() {
+    return gulp.src([
+            config.jQueryDir + '/dist/jquery-2.1.4.js',
+            config.bootstrapDir + '/assets/javascripts/bootstrap.js',
+            config.boostrapValidatorDir + '/dist/validator.js',
+            config.knockoutDir + '/dist/knockout.js',
+            config.koMappingDir + '/dist/knockout.mapping.js',
+            config.pagerJsDir + '/pager.js'
+        ])
+        .pipe(concat('vendor.js'))
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".js"
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest(config.publicDir + '/js'))
+})
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    gulp.watch(config.sassPath + '/**/*.scss', ['sass']);
+})
+
+gulp.task('build', ['uglifyPlugins']);
+
+gulp.task('default', ['bower', 'uglifyPlugins', 'fonts', 'watch']);
