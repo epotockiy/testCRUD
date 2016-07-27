d0d85b2ae64b2a9cd3d6dc5b89889abd178ef568
ArtLait <pozdnyakov_lait@mail.ru>
2016-07-27 13:35:30 +0300

    Fixed some bag with edit

--- README.md
+++
@@ -1,5 +0,0 @@
-# testCRUD
-
-Created with Vanilla, jQuery, Angular and Knockout 
-
-Rest api service - http://jsonplaceholder.typicode.com
--- app/main.js
+++
@@ -1,6 +0,0 @@
-requirejs.config({
-	paths: {"ko-component-router": "../node_modules/ko-component-router/dist/ko-component-router", "knockout": "../vendor/bower_components/knockout/dist/knockout"}
-})
-require(["mainScript", "knockout", "ko-component-router"], function(util) {
-
-});
